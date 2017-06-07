79a729e1d0292f7232e6ef332aca629a251cb5ed
Larry Reid <lcreid@jadesystems.ca>
2017-06-07 09:46:33 -0700

    Fix migrations for tags and notes.

--- db/migrate/20170605214414_create_tags.rb
+++ db/migrate/20170605214414_create_tags.rb
@@ -2,7 +2,7 @@ class CreateTags < ActiveRecord::Migration[5.1]
   def change
     create_table :tags do |t|
       t.belongs_to :taggable, polymorphic: true, index: true
-      t.text :notes
+      t.text :name
       t.timestamps
     end
   end
