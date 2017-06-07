27fe531abd49a45d743a8fc4997986e65ce21ca2
Larry Reid <lcreid@jadesystems.ca>
2017-06-07 09:47:16 -0700

    Note and Tag models.

---
+++ app/models/tag.rb
@@ -0,0 +1,6 @@
+##
+# Free-form text that can be attached to a CI or an outage to identify it
+# in a user-defined way.
+class Tag < ApplicationRecord
+  belongs_to :taggable, polymorphic: true
+end
