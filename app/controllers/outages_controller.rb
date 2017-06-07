4b29acbc4111beb799560af04c6143bd78062aaf
Larry Reid <lcreid@jadesystems.ca>
2017-06-07 15:45:54 -0700

    Create outage working.

--- app/controllers/outages_controller.rb
+++ app/controllers/outages_controller.rb
@@ -11,8 +11,7 @@ class OutagesController < ApplicationController
 
   def new
     # puts "IN NEW"
-    @outage = Outage.new
-    puts "WTF" unless @outage
+    @outage = Outage.new(outage_defaults)
   end
 
   def edit
@@ -21,11 +20,41 @@ class OutagesController < ApplicationController
   end
 
   def create
+    @outage = Outage.new(outage_params)
+    @outage.account = current_user.account
+    if @outage.save
+      redirect_to cis_path
+    else
+      puts @outage.errors.full_messages
+      render :new
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def outage_defaults
+    {
+      active: true,
+      causes_loss_of_service: true,
+      completed: false
+    }
+  end
+
+  def outage_params
+    params.require(:outage).permit(:id,
+      :account_id,
+      :active,
+      :causes_loss_of_service,
+      :completed,
+      :description,
+      :end_time,
+      :name,
+      :start_time)
   end
 end
