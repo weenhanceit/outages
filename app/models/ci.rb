9f0bc0df4d242c7eea98fc28cceeebdbbc3b8ae0
Larry Reid <lcreid@jadesystems.ca>
2017-06-06 22:17:18 -0700

    Remove extra definition of parents association.

--- app/models/ci.rb
+++ app/models/ci.rb
@@ -2,11 +2,9 @@
 # A configuration item, which can be hardware, software, service, etc.
 class Ci < ApplicationRecord
   belongs_to :account
-  has_many :parents, foreign_key: :parent_id, class_name: "CisCi", inverse_of: :parent
-  has_many :children, foreign_key: :child_id, class_name: "CisCi", inverse_of: :child
-  has_many :cis_outages, inverse_of: :ci
+  has_many :parents, foreign_key: :child_id, source: :cis_ci, inverse_of: :parent
+  has_many :children, foreign_key: :parent_id, source: :cis_ci, inverse_of: :child
   has_many :outages, through: :cis_outages
-  has_many :parents, through: :cis_cis
   has_many :notes, as: :notable
   has_many :tags, as: :taggable
   has_many :watches, as: :watched
