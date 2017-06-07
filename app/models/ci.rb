f5c9cc3326d61acabfbd95e3c5f31a75e0f4b440
Larry Reid <lcreid@jadesystems.ca>
2017-06-06 23:06:00 -0700

    This looks better.

--- app/models/ci.rb
+++ app/models/ci.rb
@@ -4,8 +4,10 @@ class Ci < ApplicationRecord
   belongs_to :account
   # Putting `inverse_of: ...` on the next two lines causes the association
   # to give incorrect answers.
-  has_many :parents, foreign_key: :child_id, class_name: "CisCi"
-  has_many :children, foreign_key: :parent_id, class_name: "CisCi"
+  has_many :parent_links, foreign_key: :child_id, class_name: "CisCi"
+  has_many :parents, through: :parent_links, class_name: "Ci"
+  has_many :child_links, foreign_key: :parent_id, class_name: "CisCi"
+  has_many :children, through: :child_links, class_name: "Ci"
   has_many :outages, through: :cis_outages
   has_many :notes, as: :notable
   has_many :tags, as: :taggable
