4a32bc93e5c65a5970afcf950d56f3bc83057cbe
Larry Reid <lcreid@jadesystems.ca>
2017-06-06 23:01:52 -0700

    CI hierarchy working but the code to use it looks funny.

--- app/models/ci.rb
+++ app/models/ci.rb
@@ -2,8 +2,10 @@
 # A configuration item, which can be hardware, software, service, etc.
 class Ci < ApplicationRecord
   belongs_to :account
-  has_many :parents, foreign_key: :child_id, source: :cis_ci, inverse_of: :parent
-  has_many :children, foreign_key: :parent_id, source: :cis_ci, inverse_of: :child
+  # Putting `inverse_of: ...` on the next two lines causes the association
+  # to give incorrect answers.
+  has_many :parents, foreign_key: :child_id, class_name: "CisCi"
+  has_many :children, foreign_key: :parent_id, class_name: "CisCi"
   has_many :outages, through: :cis_outages
   has_many :notes, as: :notable
   has_many :tags, as: :taggable
