71f21029f8457062d118d4f83bd8d818e7dcf4f4
Larry Reid <lcreid@jadesystems.ca>
2017-07-02 09:08:38 -0700

    Fragment filter test use time travel.

--- test/system/outages_filter_test.rb
+++ test/system/outages_filter_test.rb
@@ -2,19 +2,20 @@ require "application_system_test_case"
 
 class OutagesFilterTest < ApplicationSystemTestCase # rubocop:disable Metrics/ClassLength, Metrics/LineLength
   test "fragment filter" do
-    sign_in_for_system_tests(users(:basic))
-    visit outages_url
-    current_window.maximize
+    Time.use_zone(ActiveSupport::TimeZone["Samoa"]) do
+      travel_to Time.zone.local(2017, 07, 28, 10, 17, 21)
+      sign_in_for_system_tests(users(:basic))
+      visit outages_url
+      current_window.maximize
 
-    choose "watching_All"
-    fill_in "Fragment", with: "Outage B"
-    fill_in "Outages Before", with: ""
-    fill_in "Outages After", with: ""
-    click_button "Refresh"
+      choose "watching_All"
+      fill_in "Fragment", with: "Outage B"
+      click_button "Refresh"
 
-    within(".test-outages-grid") do
-      assert_text "Outage B", count: 1
-      assert_selector "tbody tr", count: 1
+      within(".test-outages-grid") do
+        assert_text "Outage B", count: 1
+        assert_selector "tbody tr", count: 1
+      end
     end
   end
 
