ccfdb09b515e57b906e6c942f12962b69f0efcd2
philcarrillo <phil.carrillo@intl-data-eng.com>
2017-07-04 23:47:57 -0400

    Added online notifications for completed events.  All tests green

--- test/models/generate_notifications_test.rb
+++ test/models/generate_notifications_test.rb
@@ -29,6 +29,36 @@ class GenerateNotificationsTest < ActiveSupport::TestCase
     end
   end
 
+  test "online notification, outage event, outage watched, completed" do
+    # Prepare a user who wants outage complete notifications,
+    # online only
+    user = users(:basic)
+    user.notify_me_on_outage_changes = false
+    user.notify_me_on_outage_complete = true
+    user.preference_notify_me_by_email = false
+    user.save
+
+    # Create an outage, and a watch on that outage
+    outage = Outage.create(test_outage_defaults.merge(account_id: user.account_id))
+    watch = Watch.create(active: true, user: user, watched: outage)
+
+    mark_all_existing_events_handled
+
+    # Generate an :outage event
+    event = Event.create(handled: false,
+                         outage_id: outage.id,
+                         text: "A test event - completed",
+                         event_type: :completed)
+
+    assert_difference "Notification.all.size" do
+      Services::GenerateNotifications.call
+      notifications = Notification.where(watch_id: watch.id)
+      assert_equal 1, notifications.size, "Wrong number of notifications generated"
+      notification = notifications.first
+      assert_equal "online", notification.notification_type
+    end
+  end
+
   test "online notification, outage event, ci watched" do
     # Prepare a user who wants outage change notifications,
     # online only
