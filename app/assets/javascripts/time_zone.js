b55d117b2d6e6dafe1eb8b29864d83b409a6eb39
Larry Reid <lcreid@jadesystems.ca>
2017-08-17 08:32:52 -0700

    Give link to settings page in time zone message.

--- app/assets/javascripts/time_zone.js
+++ app/assets/javascripts/time_zone.js
@@ -32,13 +32,16 @@ $(document).on('turbolinks:load', function() {
     browser_tz = $().get_timezone();
     // console.log('Browser time zone: ' + browser_tz);
 
+    link_to_settings = '<a href="/user/edit">preferences page</a>';
     // FIXME: Better check of browser timezone, AKA the Havana problem
     if (current_user_tz === "") {
       msg = '<p>Using the ' +
         browser_tz +
         ' time zone. ' +
         'If you want to change your time zone, ' +
-        'go to the preferences page.</p>';
+        'go to the ' +
+        link_to_settings +
+        '.</p>';
     } else if (browser_tz !== current_user_tz) {
       msg = '<p>You appear to be in ' +
         browser_tz +
@@ -46,7 +49,9 @@ $(document).on('turbolinks:load', function() {
         current_user_tz_rails +
         '. If you want to change to ' +
         browser_tz +
-        ', go to the preferences page.</p>';
+        ', go to the ' +
+        link_to_settings +
+        '.</p>';
     }
 
     if (typeof msg !== 'undefined') {
