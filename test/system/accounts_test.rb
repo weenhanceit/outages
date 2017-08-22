b7ecb2568f21eafd6211979e90bbac445d0730e6
Larry Reid <lcreid@jadesystems.ca>
2017-08-21 21:20:49 -0700

    System tests for Account.

---
+++ test/system/accounts_test.rb
@@ -0,0 +1,21 @@
+require "application_system_test_case"
+
+class AccountsTest < ApplicationSystemTestCase # rubocop:disable Metrics/ClassLength, Metrics/LineLength
+  test "create account" do
+    flunk
+  end
+
+  test "delete account" do
+    flunk
+    # Since delete is just deactivate, test that we can't navigate anywhere.
+  end
+
+  # test "undelete account" do
+  #   # Only Phil and Larry should be able to undelete accounts.
+  #   flunk
+  # end
+
+  test "change account name" do
+    flunk
+  end
+end
