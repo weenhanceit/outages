97e217be6b1adc54a9ec49b4390b0813086b97bb
Larry Reid <lcreid@jadesystems.ca>
2017-10-05 15:40:40 -0700

    Add spinner without anything visible.

--- app/assets/javascripts/submit-on-change.js
+++ app/assets/javascripts/submit-on-change.js
@@ -1,10 +1,14 @@
 $(document).on('turbolinks:load', function(e) {
   $('form.js-submit-on-change').change(function(event) {
     // console.log('form auto-submitting');
+    $(".spinner").show();
     $(this).submit();
   }).on("ajax:success", function(e) {
     // console.log('Success');
   }).on("ajax:error", function(e) {
     // console.log('Failure');
+  }).on("ajax:complete", function(e) {
+    // console.log('Complete');
+    $(".spinner").hide();
   });
 });
