3c359209dd2565e6580d88d78caa2409c0475318
Larry Reid <lcreid@jadesystems.ca>
2017-10-06 10:36:46 -0700

    Improve formatting and labels of Devise pages.

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,7 +1,8 @@
-##
-# Override the Devise error messages.
-# See: https://github.com/plataformatec/devise/wiki/Override-devise_error_messages!-for-views.
+# frozen_string_literal: true
 module DeviseHelper
+  ##
+  # Override the Devise error messages.
+  # See: https://github.com/plataformatec/devise/wiki/Override-devise_error_messages!-for-views.
   def devise_error_messages!(*attrs)
     object_error_messages(resource, *attrs) do |messages|
       <<-HTML
@@ -10,5 +11,17 @@ module DeviseHelper
       </div>
       HTML
     end
+  end
+
+  ##
+  # Append to labels for password fields.
+  def minimum_password_length
+    content_tag(:em, " (#{@minimum_password_length} characters minimum)") if @minimum_password_length
+  end
+
+  ##
+  # Create a password label.
+  def password_label(password = "Password")
+    h(password) + minimum_password_length
   end
 end
--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -38,7 +38,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   def fill_in_registration_page(email = "a@example.com", name = nil) # rubocop:disable Metrics/MethodLength, Metrics/LineLength
     fill_in_new_user_page(email)
     fill_in "Name", with: name if name
-    fill_in "Password", with: "password"
+    fill_in "user_password", with: "password"
     fill_in "Password confirmation", with: "password"
     click_button "Sign up"
     # NOTE: There's a gem to look at e-mail from Capybara tests:
