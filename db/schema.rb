4041fbd9b5215131455d32229a3fa45379b6fb91
Larry Reid <lcreid@jadesystems.ca>
2017-10-19 11:35:56 -0700

    Add full text search table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823161720) do
+ActiveRecord::Schema.define(version: 20171019182900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,6 +107,15 @@ ActiveRecord::Schema.define(version: 20170823161720) do
     t.index ["account_id"], name: "index_outages_on_account_id"
     t.index ["end_time"], name: "index_outages_on_end_time"
     t.index ["start_time"], name: "index_outages_on_start_time"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
   create_table "tags", force: :cascade do |t|
