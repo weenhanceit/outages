dc28623aec1e8a9df049edb69046b920b4942736
Larry Reid <lcreid@jadesystems.ca>
2018-03-09 10:29:20 -0800

    Format/activate outage pills properly.

--- app/assets/javascripts/outages.js
+++ app/assets/javascripts/outages.js
@@ -17,3 +17,12 @@ function openCity(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+$(document).on('turbolinks:load', function() {
+  // get current URL path and assign 'active' class based on first directory.
+  var pathname = window.location.pathname;
+  console.log(pathname);
+  // end_of_first_dir = pathname.indexOf("/", 1);
+  // pathname = pathname.slice(0, end_of_first_dir);
+  $('ul.nav.nav-pills > li.nav-item > a[href="' + pathname + '"]').addClass('active');
+});
