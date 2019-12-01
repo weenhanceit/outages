46c93e18de319c8aecee6743ae9bf5011741bdd5
Larry Reid <lcreid@jadesystems.ca>
2019-12-01 11:09:32 -0800

    Only connect when logged in

--- app/assets/javascripts/channels/notification.js
+++ app/assets/javascripts/channels/notification.js
@@ -1,17 +1,18 @@
-App.notification = App.cable.subscriptions.create("NotificationChannel", {
-  connected: function() {
-    // Called when the subscription is ready for use on the server
-    // console.log('Connected');
-  },
+App.connect_to_notification_channel = function() {
+  App.notification = App.cable.subscriptions.create("NotificationChannel", {
+    connected: function() {
+      // Called when the subscription is ready for use on the server
+      // console.log('Connected');
+    },
 
-  disconnected: function() {
-    // Called when the subscription has been terminated by the server
-    // console.log('Disconnected');
-  },
+    disconnected: function() {
+      // Called when the subscription has been terminated by the server
+      // console.log('Disconnected');
+    },
 
-  received: function(data) {
-    // Called when there's incoming data on the websocket for this channel
-    // console.log('Received notification: ' + data);
-    $('.js-notifications-list').prepend(data);
-  },
-});
+    received: function(data) {
+      // Called when there's incoming data on the websocket for this channel
+      // console.log('Received notification: ' + data);
+      $('.js-notifications-list').prepend(data);
+    },
+})};
