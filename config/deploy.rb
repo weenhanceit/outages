3d7e36d888964e95f3d0eb7e0a87abdb7bd31d50
Larry Reid <lcreid@jadesystems.ca>
2023-12-10 13:57:33 -0800

    Capistrano updated

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.17.1"
+lock "~> 3.18.0"
 
 set :application, "outages.weenhanceit.com"
 set :repo_url, "git@github.com:weenhanceit/outages.git"
