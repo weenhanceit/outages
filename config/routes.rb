c090eca067fcf8cdf7e92a0d9ddd57dcde3e98cb
Larry Reid <lcreid@jadesystems.ca>
2025-09-03 23:50:54 +0000

    Remove redundant route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get "up" => "rails/health#show", as: :rails_health_check
-
   devise_for :users, controllers: {
     invitations: :invitations,
     registrations: :registrations
